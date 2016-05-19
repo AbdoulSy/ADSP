34ff8961c4e6000dcbc0b4819ebe46af37cf2091
Abdoul Sy <dreescan@gmail.com>
2016-05-19 05:11:13 +0100

    added a Google Maps view for the geo messages section

--- mobile/src/main/java/eu/abdoulsy/eyedol/EyedolNavigation.java
+++ mobile/src/main/java/eu/abdoulsy/eyedol/EyedolNavigation.java
@@ -14,6 +14,13 @@ import android.view.Menu;
 import android.webkit.WebView;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,7 +28,10 @@ import java.io.InputStreamReader;
 import java.io.StringBufferInputStream;
 
 public class EyedolNavigation extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -111,7 +121,11 @@ public class EyedolNavigation extends AppCompatActivity
             wv.loadData(String.valueOf(total.toString()), "text/html", "utf-8");
 
         } else if (id == R.id.nav_geo_message) {
-
+            setContentView(R.layout.map_layout);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_geo_send) {
@@ -142,5 +156,24 @@ public class EyedolNavigation extends AppCompatActivity
 
 
         return true;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
