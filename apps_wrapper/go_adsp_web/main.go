889af9bf6a86e6cf9476a9f2bd0bf6e3d9cacd73
Abdoul Sy <dreescan@gmail.com>
2016-11-18 14:27:53 +0000

    refactoring the creation of a page in the main.go file

--- apps_wrapper/go_adsp_web/main.go
+++ apps_wrapper/go_adsp_web/main.go
@@ -25,89 +25,57 @@ func init() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 
-	theLinks := adspgo.CSSLinks{
-		Links: []string{
-			"/public/styles/main.css",
-			"/public/styles/layout.css",
-			"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css",
-		},
-		Page: "Index",
-	}
-
-	theScripts := adspgo.JsScripts{
-		Files: []string{
-			"/public/js/hello.js",
-			"https://code.jquery.com/jquery-2.1.1.min.js",
-			"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js",
-		},
-		Page: "Index",
-	}
-
-	myLogo := adspgo.LogoType{
-		FilePath: "/public/images/logo.png",
-		Title:    "ADSP LOGO",
-	}
-
-	homeNav := adspgo.NavElementType{
-		Name: "Home",
-		Link: "/",
-	}
-
-	projectsNav := adspgo.NavElementType{
-		Name: "Projects",
-		Link: "/projects",
-	}
-
-	visualisationNav := adspgo.NavElementType{
-		Name: "Visualisation",
-		Link: "/visualisation",
-	}
-
-	myNav := adspgo.NavType{
-		Elements: []adspgo.NavElementType{homeNav, projectsNav, visualisationNav},
-	}
-
 	myPage := adspgo.PageType{
 		ID:          "aria-abdoulsy-eu/adsp/home",
 		Title:       "The Home Page",
-		Description: "This Page describe the current State of the Team",
+		Description: "This Page describe the current state of the Team",
 	}
-
-	c := adspgo.PageLayout{
-		Contents: "I am Dog",
-		Styles:   theLinks,
-		Scripts:  theScripts,
-		Logo:     myLogo,
-		Nav:      myNav,
-		Page:     myPage,
-	}
+	c, er := adspgo.BuildBasicLayoutWithPage(myPage)
 
 	err := tpl.ExecuteTemplate(w, "layout", c)
-	if err != nil {
+	if err != nil || er != nil {
 		log.Println(err)
 	}
 }
 
 func projects(w http.ResponseWriter, req *http.Request) {
 
-	err := tpl.ExecuteTemplate(w, "layout", 44)
-	if err != nil {
+	projectPage := adspgo.PageType{
+		ID:          "aria-abdoulsy-eu/adsp/projects",
+		Title:       "The Project Page",
+		Description: "This Page shows the Current Projects of the Team",
+	}
+	c, er := adspgo.BuildBasicLayoutWithPage(projectPage)
+	err := tpl.ExecuteTemplate(w, "layout", c)
+	if err != nil || er != nil {
 		log.Println(err)
 	}
 }
 
 func projectBoards(w http.ResponseWriter, req *http.Request) {
 
-	err := tpl.ExecuteTemplate(w, "layout", 44)
-	if err != nil {
+	myPage := adspgo.PageType{
+		ID:          "aria-abdoulsy-eu/adsp/projects-boards",
+		Title:       "The Project Boards Page",
+		Description: "This Page shows the Boards a-la-trello of the Team",
+	}
+	c, er := adspgo.BuildBasicLayoutWithPage(myPage)
+	err := tpl.ExecuteTemplate(w, "layout", c)
+	if err != nil || er != nil {
 		log.Println(err)
 	}
 }
 
 func visualisation(w http.ResponseWriter, req *http.Request) {
 
-	err := tpl.ExecuteTemplate(w, "layout", 45)
-	if err != nil {
+	myPage := adspgo.PageType{
+		ID:          "aria-abdoulsy-eu/adsp/visualisation",
+		Title:       "The Visualisation Page",
+		Description: "This Page shows various visualisations for the Team",
+	}
+	c, er := adspgo.BuildBasicLayoutWithPage(myPage)
+	err := tpl.ExecuteTemplate(w, "layout", c)
+	if err != nil || er != nil {
 		log.Println(err)
 	}
 }
