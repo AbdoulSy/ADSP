3cf821d68fcd28ed1f9682f241a6402d6601aa38
Abdoul Sy <dreescan@gmail.com>
2016-11-23 19:28:03 +0000

    Installed Leasot which can extract TODOS > json, removed fixme which couldn't

--- bin/fixme.js
+++
@@ -1,9 +0,0 @@
-var fixme = require('fixme');
-// All values below are Fixme default values unless otherwise overridden here. 
-fixme({
-  path:                 process.cwd() + '/apps_wrapper/projects',
-  ignored_directories:  ['node_modules/**', '.git/**', '.hg/**'],
-  file_patterns:        ['**/*.js', 'Makefile', '**/*.sh'],
-  file_encoding:        'utf8',
-  line_length_limit:    1000
-});
---
+++ bin/leasot.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var leasot = require('leasot');
+
+module.exports = function (filePath) {
+
+  var contents = fs.readFileSync(filePath, 'utf8');
+  // get the filetype of the file, or force a special parser 
+  var filetype = path.extname(filePath);
+  // add file for better reporting 
+  var file = path.basename(filePath);
+  var todos = leasot.parse({ext: filetype, content: contents, fileName: file});
+ 
+   // -> todos now contains the array of todos/fixme parsed 
+ 
+  var output = leasot.reporter(todos, {
+      reporter: 'json',
+      spacing: 2
+  });
+ 
+  console.log(output);
+  return output;
+}; 
