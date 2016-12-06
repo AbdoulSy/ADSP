2cf0019867b79c5137791cba9f76b3324a05aef2
Abdoul Sy <dreescan@gmail.com>
2016-12-06 09:01:15 +0100

    using a real ontology on the server reupload with mlcp

--- apps_wrapper/file_analyser/env/env.js
+++ apps_wrapper/file_analyser/env/env.js
@@ -3,7 +3,7 @@ var dev =  {
   host: "localhost",     // The host against which queries will be run
   port: 8000,            // By default port 8000 accepts Client API requests
   user: "writer",        // Our newly-created user with at least the rest-writer role
-  password: "stupid password",  // writer's password
+  password: "fiokdelkof",  // writer's password
   authType: "DIGEST"     // The default auth
 };
 
@@ -23,7 +23,7 @@ var triple = {
   host: "localhost",     // The host against which queries will be run
   port: 8000,            // By default port 8000 accepts Client API requests
   user: "writer",        // Our newly-created user with at least the rest-writer role
-  password: "stupid password",  // writer's password
+  password: "fiokdelkof",  // writer's password
   authType: "DIGEST"     // The default auth
 };
 
