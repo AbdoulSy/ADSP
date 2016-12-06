e571a05bbb7bf3376d1c2bdf10ef3cd1a58a4f7e
Abdoul Sy <dreescan@gmail.com>
2016-12-06 05:13:58 +0100

    created a triple reader to be injested with data

---
+++ apps_wrapper/file_analyser/bin/get-triples.js
@@ -0,0 +1,38 @@
+/* jshint esversion: 6 */
+var _ = require('lodash');
+const my      = require(__dirname + '/../env/env');
+const MarklogicClient = require('marklogic');
+//connecting to Marklogic
+var db = MarklogicClient.createDatabaseClient(my.triple);
+var qb = MarklogicClient.queryBuilder;
+
+console.log("\n users marklogic connection established");
+
+db.graphs.list('text/uri-list')
+  .result(
+    function(response) {
+      for (var uri of response.split('\n')) {
+        console.log(uri);
+      }
+    },
+    function(error) { console.log(JSON.stringify(error)); }
+);
+
+
+db.graphs.sparql({
+  contentType: 'application/sparql-results+json',
+  query: 'PREFIX adsp: <http://aria.abdoulsy.eu/ontology/ADSP/Web_Project#>'+
+  'SELECT ?s ?p WHERE '+
+  '{'+
+  '?s adsp:firstname "Abdoul".'+
+  '?s a adsp:Member .'+
+  '?s adsp:has_active_email ?email .'+
+  '}',
+  rulesets: 'subPropertyOf.rules',
+  defaultRuleSets: 'exclude'
+}).result(
+    function(response) {
+      console.dir(response.results.bindings);
+    },
+    function(error) { console.log(JSON.stringify(error)); }
+);;
--- apps_wrapper/file_analyser/env/env.js
+++ apps_wrapper/file_analyser/env/env.js
@@ -7,7 +7,7 @@ var dev =  {
   authType: "DIGEST"     // The default auth
 };
 
-// Another connection. Change the module.exports below to 
+// Another connection. Change the module.exports below to
 // use it without having to change consuming code.
 var test =  {
   database: "Documents",
@@ -18,6 +18,16 @@ var test =  {
   authType: "DIGEST"
 };
 
+var triple = {
+  database: "adsp-db", // Each connection can specify its own database
+  host: "localhost",     // The host against which queries will be run
+  port: 8000,            // By default port 8000 accepts Client API requests
+  user: "writer",        // Our newly-created user with at least the rest-writer role
+  password: "stupid password",  // writer's password
+  authType: "DIGEST"     // The default auth
+};
+
 module.exports = {
-  connection: dev       // Export the development connection
+  connection: dev,       // Export the development connection
+  triple: triple
 };
