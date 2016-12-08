699eaba87fcdc06ca1091791d3885cda0d4dabdc
Abdoul Sy <dreescan@gmail.com>
2016-12-08 14:14:09 +0000

    using environment variables to process connections

--- apps_wrapper/file_analyser/env/env.js
+++ apps_wrapper/file_analyser/env/env.js
@@ -1,10 +1,10 @@
 var dev =  {
-  database: "Documents", // Each connection can specify its own database
-  host: "localhost",     // The host against which queries will be run
-  port: 8000,            // By default port 8000 accepts Client API requests
-  user: "writer",        // Our newly-created user with at least the rest-writer role
-  password: "fiokdelkof",  // writer's password
-  authType: "DIGEST"     // The default auth
+  database: process.env.DOCUMENTS_DB_NAME, // Each connection can specify its own database
+  host: process.env.DOCUMENTS_DB_HOST, // The host against which queries will be run
+  port: process.env.DOCUMENTS_DB_PORT, // By default port 8000 accepts Client API requests
+  user: process.env.DOCUMENT_DB_USER, // Our newly-created user with at least the rest-writer role
+  password: process.env.DOCUMENTS_DB_PASSWORD, // writer's password
+  authType: process.env.DOCUMENTS_DB_AUTHTYPE // The default auth
 };
 
 // Another connection. Change the module.exports below to
@@ -19,15 +19,15 @@ var test =  {
 };
 
 var triple = {
-  database: "adsp-db", // Each connection can specify its own database
-  host: "localhost",     // The host against which queries will be run
-  port: 8000,            // By default port 8000 accepts Client API requests
-  user: "writer",        // Our newly-created user with at least the rest-writer role
-  password: "fiokdelkof",  // writer's password
-  authType: "DIGEST"     // The default auth
+  database: process.env.TRIPLE_DB_NAME, // Each connection can specify its own database
+  host: process.env.TRIPLE_DB_HOST, // The host against which queries will be run
+  port: process.env.TRIPLE_DB_ENV, // By default port 8000 accepts Client API requests
+  user: process.env.TRIPLE_DB_USER, // Our newly-created user with at least the rest-writer role
+  password: process.env.TRIPLE_DB_PASSWORD,// writer's password
+  authType: process.env.TRIPLE_DB_AUTHTYPE // The default auth
 };
 
 module.exports = {
-  connection: dev,       // Export the development connection
+  connection: dev, // Export the development connection
   triple: triple
 };
