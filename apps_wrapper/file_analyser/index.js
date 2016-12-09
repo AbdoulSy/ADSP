71f8d9d993d2968be90ddd0eabb57a33266e2fef
Abdoul Sy <dreescan@gmail.com>
2016-12-09 14:00:13 +0000

    better spacing

--- apps_wrapper/file_analyser/index.js
+++ apps_wrapper/file_analyser/index.js
@@ -18,44 +18,40 @@ app.get('/', function (req, res) {
     try {
       processFn(Conn, function callback(docs) {
           let docsContents = docs[0].content;
-	  let stringDocs = JSON.stringify(docsContents);
-	  res.send(stringDocs);
+          let stringDocs = JSON.stringify(docsContents);
+          res.send(stringDocs);
       });
     } catch (e) {
         console.log({
-	   exception: e
-	});
+          exception: e
+        });
     }
 });
 
 app.get('/projects', function (req, res) {
     try {
-        getProjectsFn(Conn, function (projects) {
-		res.send(projects);
-	});
+      getProjectsFn(Conn, function (projects) {
+        res.send(projects);
+      });
     } catch (e) {
         console.log({
-	       exception: e
-	      });
+         exception: e
+        });
     }
-
 });
 
 app.get('/function-number', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', false);
     try {
         getFnNbFn(Conn, function (data) {
-    res.send(data);
+        res.send(data);
   });
     } catch (e) {
         console.log({
@@ -69,7 +65,6 @@ app.get('/current-user', function (req, res){
     getUserFn(Conn, function(user){
        res.send(user);
     });
-
 });
 
 app.get('/commit-history', function (req, res) {
