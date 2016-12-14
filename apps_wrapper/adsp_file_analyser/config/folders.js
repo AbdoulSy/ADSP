14c605aeed58887e39f00b503d513acefee3612c
Abdoul Sy <dreescan@gmail.com>
2016-12-14 23:00:18 +0000

    updated all submodules at once from the parent project tree

--- apps_wrapper/adsp_file_analyser/config/folders.js
+++ apps_wrapper/adsp_file_analyser/config/folders.js
@@ -1,6 +1,6 @@
 module.exports = {
-  file_analyser: './apps_wrapper/file_analyser',
-  Eyedol: './apps_wrapper/Eyedol',
-  Glacius: './apps_wrapper/Glacius',
-  ADSP_Dashboard: './apps_wrapper/go_apps/src/github.com/AbdoulSy/web'
+  file_analyser: './apps_wrapper/adsp_file_analyser',
+  Eyedol: './apps_wrapper/eyedol',
+  Glacius: './apps_wrapper/glacius',
+  ADSP_Dashboard: './apps_wrapper/go_apps/src/github.com/AbdoulSy/adsp-dashboard'
 };
